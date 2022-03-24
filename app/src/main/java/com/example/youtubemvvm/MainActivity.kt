aaa6394a9b53408eeec0a33283fc2a39094e75e8
thuan-dd <ducthuan232tn@gmail.com>
2022-03-24 23:09:08 +0700

    + delete line

--- app/src/main/java/com/example/youtubemvvm/MainActivity.kt
+++ app/src/main/java/com/example/youtubemvvm/MainActivity.kt
@@ -19,6 +19,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 //        val navController = Navigation.findNavController(this,R.id.homeFragment)
 //        binding.bottomNavigation.setupWithNavController(
