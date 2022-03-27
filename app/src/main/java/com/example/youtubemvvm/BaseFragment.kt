679a34e352308e19bfa5a404851e7902bc0e2aae
thuan-dd <ducthuan232tn@gmail.com>
2022-03-27 22:10:35 +0700

    searchView commit

---
+++ app/src/main/java/com/example/youtubemvvm/BaseFragment.kt
@@ -0,0 +1,14 @@
+package com.example.youtubemvvm
+
+import androidx.fragment.app.Fragment
+
+open class BaseFragment: Fragment() {
+
+    fun showProgressBar(){
+
+    }
+
+    fun hideProgressBar(){
+
+    }
+}
--- app/src/main/java/com/example/youtubemvvm/home/data/datasource/VideoService.kt
+++ app/src/main/java/com/example/youtubemvvm/home/data/datasource/VideoService.kt
@@ -7,5 +7,5 @@ import retrofit2.http.Query
 
 interface VideoService {
     @GET("v3/search?part=snippet&maxResults=50&key=AIzaSyBcXwHnDNxMx7G3vXR1b0b4K9yJz0q1oUE")
-    suspend fun getVideos(@Query("q") searchKey: String = ""): Response<Videos>
+    suspend fun getVideos(@Query("q") searchKey: String? = ""): Response<Videos>
 }
