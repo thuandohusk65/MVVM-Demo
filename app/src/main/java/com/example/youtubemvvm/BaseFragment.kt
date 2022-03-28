b3f4538841aa6ba39fb021c4260ca08e8e79aac8
Nguyen Viet Nhan <nhannguyenviet1@gmail.com>
2022-03-28 09:59:53 +0700

    alertLoading

--- app/src/main/java/com/example/youtubemvvm/BaseFragment.kt
+++ app/src/main/java/com/example/youtubemvvm/BaseFragment.kt
@@ -1,14 +1,20 @@
 package com.example.youtubemvvm
 
+import android.app.AlertDialog
+import android.os.Handler
 import androidx.fragment.app.Fragment
 
-open class BaseFragment: Fragment() {
-
+open class BaseFragment(): Fragment() {
+    lateinit var isDialog: AlertDialog
     fun showProgressBar(){
-
+        val builder = AlertDialog.Builder(activity)
+        builder.setView(R.layout.loading)
+        isDialog = builder.create()
+        isDialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        isDialog.show()
     }
 
     fun hideProgressBar(){
-
+        isDialog.dismiss()
     }
 }
--- app/src/main/java/com/example/youtubemvvm/home/data/datasource/VideoService.kt
+++ app/src/main/java/com/example/youtubemvvm/home/data/datasource/VideoService.kt
@@ -6,6 +6,6 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface VideoService {
-    @GET("v3/search?part=snippet&maxResults=50&key=AIzaSyBcXwHnDNxMx7G3vXR1b0b4K9yJz0q1oUE")
+    @GET("v3/search?part=snippet&maxResults=50&key=AIzaSyCAdhoyzLoGK28re--WSCCVcaXvyJnIdHw")
     suspend fun getVideos(@Query("q") searchKey: String? = ""): Response<Videos>
 }
