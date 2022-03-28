36f31f90024faf5123b5eae077623e9a354866f6
Nguyen Viet Nhan <nhannguyenviet1@gmail.com>
2022-03-28 23:25:17 +0700

    webview + loadingAlert

--- app/src/main/java/com/example/youtubemvvm/home/data/datasource/VideoService.kt
+++ app/src/main/java/com/example/youtubemvvm/home/data/datasource/VideoService.kt
@@ -6,6 +6,6 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface VideoService {
-    @GET("v3/search?part=snippet&maxResults=50&key=AIzaSyCAdhoyzLoGK28re--WSCCVcaXvyJnIdHw")
+    @GET("v3/search?part=snippet&maxResults=50&key=AIzaSyBcXwHnDNxMx7G3vXR1b0b4K9yJz0q1oUE")
     suspend fun getVideos(@Query("q") searchKey: String? = ""): Response<Videos>
 }
